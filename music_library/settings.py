781b22a46875c90743afcf8a778021f2fc7c17a2
austinw298 <austinw298@gmail.com>
2023-02-02 18:34:44 -0700

    Added POST to function

--- music_library/settings.py
+++ music_library/settings.py
@@ -75,6 +75,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'music_library.wsgi.application'
 
 
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Song
+from .serializers import MusicSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
-    # if request.method == 'GET':
-        
-
-    return Response('ok')
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = MusicSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = MusicSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()    
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
