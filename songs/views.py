704e789d97c0697cb3cfde97f9f0d2ef9ad548c6
austinw298 <austinw298@gmail.com>
2023-02-02 19:51:16 -0700

    Fixed song_detail function for GET, PUT, and DELETE

--- songs/views.py
+++ songs/views.py
@@ -16,3 +16,22 @@ def songs_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()    
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#don't forget when you delete a song the ID doesnt move down, currently when getting
+#song by ID, 
+#the third song in the list, "Good Days", has ID of 4 not 3.
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def song_detail(request, pk):
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
+        serializer = MusicSerializer(song)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = MusicSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
